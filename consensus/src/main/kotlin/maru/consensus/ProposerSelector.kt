1b29aebfadf0ccb7db18915037db13972c00cbaa
Jason Frame <jason.frame@consensys.net>
2025-03-24 17:07:08 +1000

    proposer selector

--- consensus/src/main/kotlin/maru/consensus/ProposerSelector.kt
+++ consensus/src/main/kotlin/maru/consensus/ProposerSelector.kt
@@ -17,8 +17,28 @@ package maru.consensus
 
 import maru.core.BeaconBlockHeader
 import maru.core.Validator
+import maru.database.BeaconChain
+import org.apache.tuweni.bytes.Bytes
+import org.hyperledger.besu.consensus.common.bft.ConsensusRoundIdentifier
+import org.hyperledger.besu.consensus.common.bft.blockcreation.ProposerSelector.selectProposerForRound
+import org.hyperledger.besu.datatypes.Address
 import tech.pegasys.teku.infrastructure.async.SafeFuture
 
 interface ProposerSelector {
   fun getProposerForBlock(header: BeaconBlockHeader): SafeFuture<Validator>
+}
+
+class ProposerSelectorImpl(
+  private val beaconChain: BeaconChain,
+  private val validatorProvider: ValidatorProvider
+) : ProposerSelector {
+  override fun getProposerForBlock(header: BeaconBlockHeader): SafeFuture<Validator> {
+    val roundIdentifier = ConsensusRoundIdentifier(header.number.toLong(), header.round.toInt())
+    val beaconBlock = beaconChain.getSealedBeaconBlock(header.parentRoot) ?: return SafeFuture.failedFuture(IllegalStateException("Parent state not found"))
+    val prevBlockProposer = Address.wrap(Bytes.wrap(beaconBlock.beaconBlock.beaconBlockHeader.proposer.address))
+    val validatorsForRound = validatorProvider.getValidatorsForBlock(header).get().map { Address.wrap(Bytes.wrap(it.address)) }
+    val proposer = selectProposerForRound(roundIdentifier, prevBlockProposer, validatorsForRound, true)
+    return SafeFuture.completedFuture(Validator(proposer.toArray()));
+  }
+
 }
