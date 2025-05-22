fd89327f0d8acdf1ad72ddf8138f574236254651
Jason Frame <jason.frame@consensys.net>
2025-05-22 14:31:48 +1000

    try using isolation to improve test flakiness

--- app/src/test/kotlin/maru/app/MaruQbftTest.kt
+++ app/src/test/kotlin/maru/app/MaruQbftTest.kt
@@ -46,11 +46,11 @@ import org.hyperledger.besu.tests.acceptance.dsl.transaction.net.NetTransactions
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
-import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
+import org.junit.jupiter.api.parallel.Isolated
 
-@Tag("nonParallel")
+@Isolated
 class MaruQbftTest {
   private lateinit var cluster: Cluster
   private lateinit var besuNode: BesuNode
