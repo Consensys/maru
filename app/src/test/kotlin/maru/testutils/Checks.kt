11360dbd3256f7677ccc62affee41f35f96266be
Roman <4833306+Filter94@users.noreply.github.com>
2025-06-02 12:54:51 +0200

    Spotless

--- app/src/test/kotlin/maru/testutils/Checks.kt
+++ app/src/test/kotlin/maru/testutils/Checks.kt
@@ -1,11 +1,26 @@
+/*
+   Copyright 2025 Consensys Software Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package maru.testutils
 
+import java.math.BigInteger
 import maru.testutils.besu.BesuFactory
 import org.assertj.core.api.Assertions
 import org.hyperledger.besu.tests.acceptance.dsl.node.BesuNode
 import org.web3j.protocol.core.DefaultBlockParameter
 import org.web3j.protocol.core.methods.response.EthBlock
-import java.math.BigInteger
 
 object Checks {
   fun BesuNode.getMinedBlocks(blocksMined: Int): List<EthBlock.Block> =
@@ -27,7 +42,8 @@ object Checks {
     timestampsSeconds.reduceIndexed { index, prevTimestamp, timestamp ->
       Assertions.assertThat(prevTimestamp).isLessThan(timestamp)
       val actualBlockTime = timestamp - prevTimestamp
-      Assertions.assertThat(actualBlockTime)
+      Assertions
+        .assertThat(actualBlockTime)
         .withFailMessage("Timestamps: $timestampsSeconds")
         .isEqualTo(BesuFactory.MIN_BLOCK_TIME)
       timestamp
