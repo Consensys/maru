7c4401f787ab7db97dd09c235e4ad05c656784b6
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-07-16 17:23:49 +0100

    fix besu, maru orchestration to allow synch blocks

--- helm/helm.mk
+++ helm/helm.mk
@@ -3,9 +3,10 @@ clean-releases:
 	-@helm --kubeconfig $(KUBECONFIG) uninstall besu-sequencer
 	-@helm --kubeconfig $(KUBECONFIG) uninstall besu-follower
 	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-validator
-	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower
 	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower-0
 	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower-1
+	KUBECONFIG=$(KUBECONFIG) kubectl delete pvc --all
+	KUBECONFIG=$(KUBECONFIG) kubectl delete pv --all
 
 redeploy-besu:
 		@echo "Redeploying Besu"
@@ -18,15 +19,22 @@ redeploy-besu:
 redeploy-maru:
 	@echo "Redeploying Maru"
 	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-validator
-	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower
+	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower-0
+	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower-1
 	@sleep 2 # Wait for a second to ensure the previous release is fully uninstalled
 	@echo "Deploying Maru Followers"
 	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-0 ./charts/maru --force -f ./charts/maru/values.yaml -f ./values/maru-local-dev-follower-0.yaml
 	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-1 ./charts/maru --force -f ./charts/maru/values.yaml -f ./values/maru-local-dev-follower-1.yaml
+	@sleep 3 # wait for followers to start
 	@echo "Deploying Maru Validator"
 	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-validator ./charts/maru --force -f ./charts/maru/values.yaml -f ./values/maru-local-dev-validator.yaml
 
 redeploy:
 	@echo "Redeploying Besu and Maru"
+	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) clean-releases
 	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) redeploy-besu
+	@sleep 10
+	# Wait for Besu to be fully deployed,
+	# otherwise Maru will fail to start because it cannot connect to Besu
+	# then will miss P2P messages from validator
 	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) redeploy-maru
