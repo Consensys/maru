675221311157c0144e522bfa74a3db486499c7d5
Gaurav Ahuja <gauravahuja9@gmail.com>
2025-07-17 19:45:38 -0400

    Interfaces for Syncing

---
+++ core/src/main/kotlin/maru/syncing/SyncStatus.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * This file is dual-licensed under either the MIT license or Apache License 2.0.
+ * See the LICENSE-MIT and LICENSE-APACHE files in the repository root for details.
+ *
+ * SPDX-License-Identifier: MIT OR Apache-2.0
+ */
+package maru.syncing
+
+import java.util.function.Supplier
+
+data class SyncStatus(
+  val isSyncing: Boolean,
+  val headBeaconBlockNumber: ULong,
+  val syncDistance: ULong, // (headBeaconBlockNumber - latestBeaconBlockNumber_on_node)
+  val eLSynced: Boolean, // true if the execution layer is synced with the beacon chain
+)
+
+interface SyncStatusProvider : Supplier<SyncStatus>
---
+++ syncing/src/main/kotlin/NewBlocksPipeline.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * This file is dual-licensed under either the MIT license or Apache License 2.0.
+ * See the LICENSE-MIT and LICENSE-APACHE files in the repository root for details.
+ *
+ * SPDX-License-Identifier: MIT OR Apache-2.0
+ */
+import maru.core.Protocol
+import maru.core.SealedBeaconBlock
+
+interface NewBlocksPipeline : Protocol {
+  fun poll(): SealedBeaconBlock?
+
+  fun peek(): SealedBeaconBlock?
+}
