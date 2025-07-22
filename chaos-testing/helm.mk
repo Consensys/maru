a4f1eebd925a4504f93745ffb440e6615b9dd94c
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-07-22 11:24:33 +0100

    move helm inside chaos-testing

--- chaos-testing/helm.mk
+++ chaos-testing/helm.mk
@@ -1,4 +1,4 @@
-clean-releases:
+helm-clean-releases:
 	@echo "Cleaning up Helm releases"
 	-@helm --kubeconfig $(KUBECONFIG) uninstall besu-sequencer
 	-@helm --kubeconfig $(KUBECONFIG) uninstall besu-follower
@@ -8,33 +8,33 @@ clean-releases:
 	KUBECONFIG=$(KUBECONFIG) kubectl delete pvc --all
 	KUBECONFIG=$(KUBECONFIG) kubectl delete pv --all
 
-redeploy-besu:
+helm-redeploy-besu:
 		@echo "Redeploying Besu"
 		-@helm --kubeconfig $(KUBECONFIG) uninstall besu-sequencer
 		-@helm --kubeconfig $(KUBECONFIG) uninstall besu-follower
 		@sleep 3 # Wait for a second to ensure the previous release is fully uninstalled
-		@helm --kubeconfig $(KUBECONFIG) upgrade --install besu-sequencer ./charts/besu --force -f ./charts/besu/values.yaml -f ./values/besu-local-dev-sequencer.yaml
-		@helm --kubeconfig $(KUBECONFIG) upgrade --install besu-follower ./charts/besu --force -f ./charts/besu/values.yaml -f ./values/besu-local-dev-follower.yaml
+		@helm --kubeconfig $(KUBECONFIG) upgrade --install besu-sequencer ./helm/charts/besu --force -f ./helm/charts/besu/values.yaml -f ./helm/values/besu-local-dev-sequencer.yaml
+		@helm --kubeconfig $(KUBECONFIG) upgrade --install besu-follower ./helm/charts/besu --force -f ./helm/charts/besu/values.yaml -f ./helm/values/besu-local-dev-follower.yaml
 
-redeploy-maru:
+helm-redeploy-maru:
 	@echo "Redeploying Maru"
 	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-validator
 	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower-0
 	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower-1
 	@sleep 2 # Wait for a second to ensure the previous release is fully uninstalled
 	@echo "Deploying Maru Followers"
-	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-0 ./charts/maru --force -f ./charts/maru/values.yaml -f ./values/maru-local-dev-follower-0.yaml
-	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-1 ./charts/maru --force -f ./charts/maru/values.yaml -f ./values/maru-local-dev-follower-1.yaml
+	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-0 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-follower-0.yaml
+	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-1 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-follower-1.yaml
 	@sleep 3 # wait for followers to start
 	@echo "Deploying Maru Validator"
-	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-validator ./charts/maru --force -f ./charts/maru/values.yaml -f ./values/maru-local-dev-validator.yaml
+	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-validator ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-validator.yaml
 
-redeploy:
+helm-redeploy-maru-and-besu:
 	@echo "Redeploying Besu and Maru"
-	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) clean-releases
-	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) redeploy-besu
+	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) helm-clean-releases
+	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) helm-redeploy-besu
 	@sleep 10
 	# Wait for Besu to be fully deployed,
 	# otherwise Maru will fail to start because it cannot connect to Besu
 	# then will miss P2P messages from validator
-	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) redeploy-maru
+	$(MAKE) -f $(firstword $(MAKEFILE_LIST)) helm-redeploy-maru
