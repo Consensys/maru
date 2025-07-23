941007b01de52c8dd9d610141a7170b0a841b987
Jason Frame <jason.frame@consensys.net>
2025-07-23 15:40:28 +1000

    keep existing ULongExtension tests

--- jvm-libs/extensions/src/test/kotlin/ULongExtensionsTest.kt
+++ jvm-libs/extensions/src/test/kotlin/ULongExtensionsTest.kt
@@ -6,12 +6,36 @@
  *
  * SPDX-License-Identifier: MIT OR Apache-2.0
  */
-package maru.extensions
-
+package linea.kotlin
+import maru.extensions.clampedAdd
+import maru.extensions.toBytes32
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class ULongExtensionsTest {
+  fun String.toByteArray(): ByteArray =
+    this
+      .removePrefix("0x")
+      .chunked(2)
+      .map { it.toInt(16).toByte() }
+      .toByteArray()
+
+  val uLongParsingTestCases =
+    mapOf(
+      1UL to "0x0000000000000000000000000000000000000000000000000000000000000001".toByteArray(),
+      16UL to "0x0000000000000000000000000000000000000000000000000000000000000010".toByteArray(),
+      0xABCDEF1234567890UL to "0x000000000000000000000000000000000000000000000000abcdef1234567890".toByteArray(),
+      ULong.MIN_VALUE to "0x0000000000000000000000000000000000000000000000000000000000000000".toByteArray(),
+      ULong.MAX_VALUE to "0x000000000000000000000000000000000000000000000000ffffffffffffffff".toByteArray(),
+    )
+
+  @Test
+  fun toBytes32() {
+    uLongParsingTestCases.forEach { (number: ULong, byteArray: ByteArray) ->
+      assertThat(number.toBytes32()).isEqualTo(byteArray)
+    }
+  }
+
   @Test
   fun `clampedAdd returns sum when no overflow`() {
     val a = 10uL
