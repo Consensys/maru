e1cff4c9f6574b9562640e7a22103beb2d00a7df
Gaurav Ahuja <gauravahuja9@gmail.com>
2025-07-22 20:25:36 -0400

    Update interfaces based on Tuesday's discussion

--- p2p/src/main/kotlin/maru/p2p/MaruPeer.kt
+++ p2p/src/main/kotlin/maru/p2p/MaruPeer.kt
@@ -12,7 +12,6 @@ import java.util.Optional
 import java.util.concurrent.atomic.AtomicReference
 import maru.p2p.messages.BeaconBlocksByRangeRequest
 import maru.p2p.messages.BeaconBlocksByRangeResponse
-import maru.p2p.messages.LatestBeaconStateResponse
 import maru.p2p.messages.Status
 import maru.p2p.messages.StatusMessageFactory
 import org.apache.logging.log4j.LogManager
@@ -40,8 +39,6 @@ interface MaruPeer : Peer {
     startBlockNumber: ULong,
     count: ULong,
   ): SafeFuture<BeaconBlocksByRangeResponse>
-
-  fun getLatestBeaconState(): SafeFuture<LatestBeaconStateResponse>
 }
 
 interface MaruPeerFactory {
@@ -94,10 +91,6 @@ class DefaultMaruPeer(
     val message = Message(RpcMessageType.BEACON_BLOCKS_BY_RANGE, Version.V1, request)
     return sendRpcMessage(message, rpcMethods.beaconBlocksByRange())
       .thenApply { responseMessage -> responseMessage.payload }
-  }
-
-  override fun getLatestBeaconState(): SafeFuture<LatestBeaconStateResponse> {
-    TODO("Not yet implemented")
   }
 
   fun <TRequest : Message<*, RpcMessageType>, TResponse : Message<*, RpcMessageType>> sendRpcMessage(
