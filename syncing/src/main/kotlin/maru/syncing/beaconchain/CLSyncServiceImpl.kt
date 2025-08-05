296c413ecd39fe42f375c88849b467e1791d6287
Roman <4833306+Filter94@users.noreply.github.com>
2025-08-05 22:58:57 +0400

    Trying to remove the synchronization block

--- syncing/src/main/kotlin/maru/syncing/beaconchain/CLSyncServiceImpl.kt
+++ syncing/src/main/kotlin/maru/syncing/beaconchain/CLSyncServiceImpl.kt
@@ -38,7 +38,7 @@ class CLSyncServiceImpl(
   metricsFacade: MetricsFacade,
 ) : CLSyncService,
   LongRunningService {
-  private val log: Logger = LogManager.getLogger(this::class.java)
+  private val log: Logger = LogManager.getLogger(this.javaClass)
   private val beaconChainPipeline = AtomicReference<BeaconChainPipeline?>(null)
   private val syncTarget = AtomicReference(0UL)
   private val started = AtomicBoolean(false)
@@ -62,18 +62,16 @@ class CLSyncServiceImpl(
     )
 
   override fun setSyncTarget(syncTarget: ULong) {
-    synchronized(beaconChainPipeline) {
-      check(started.get()) { "Sync service must be started before setting sync target" }
+    check(started.get()) { "Sync service must be started before setting sync target" }
 
-      val oldTarget = this.syncTarget.getAndSet(syncTarget)
-      if (oldTarget != syncTarget) {
-        log.info("Syncing target updated from {} to {}", oldTarget, syncTarget)
-      }
+    val oldTarget = this.syncTarget.getAndSet(syncTarget)
+    if (oldTarget != syncTarget) {
+      log.info("Syncing target updated from {} to {}", oldTarget, syncTarget)
+    }
 
-      // If the pipeline is already running, we don't need to start a new one
-      if (beaconChainPipeline.get() == null) {
-        startSync()
-      }
+    // If the pipeline is already running, we don't need to start a new one
+    if (beaconChainPipeline.get() == null) {
+      startSync()
     }
   }
 
@@ -84,28 +82,26 @@ class CLSyncServiceImpl(
 
     if (beaconChainPipeline.compareAndSet(null, pipeline)) {
       pipeline.pipline.start(executorService).handle { _, ex ->
-        synchronized(beaconChainPipeline) {
-          if (ex != null && ex !is CancellationException) {
-            log.error("Sync pipeline failed, restarting", ex)
-            pipelineRestartCounter.increment()
-            if (beaconChainPipeline.compareAndSet(pipeline, null)) {
-              startSync()
-            }
-          } else {
-            val completedSyncTarget = pipeline.target()
-            beaconChainPipeline.compareAndSet(pipeline, null)
-            log.info("Sync completed completedSyncTarget={} syncTarget={}", completedSyncTarget, syncTarget.get())
+        if (ex != null && ex !is CancellationException) {
+          log.error("Sync pipeline failed, restarting", ex)
+          pipelineRestartCounter.increment()
+          if (beaconChainPipeline.compareAndSet(pipeline, null)) {
+            startSync()
+          }
+        } else {
+          val completedSyncTarget = pipeline.target()
+          beaconChainPipeline.compareAndSet(pipeline, null)
+          log.info("Sync completed completedSyncTarget={} syncTarget={}", completedSyncTarget, syncTarget.get())
 
-            if (completedSyncTarget < syncTarget.get()) {
-              log.info(
-                "Starting new sync as current target {} is higher than completed {}",
-                syncTarget.get(),
-                completedSyncTarget,
-              )
-              startSync()
-            } else {
-              syncCompleteHanders.notifySubscribers(completedSyncTarget)
-            }
+          if (completedSyncTarget < syncTarget.get()) {
+            log.info(
+              "Starting new sync as current target {} is higher than completed {}",
+              syncTarget.get(),
+              completedSyncTarget,
+            )
+            setSyncTarget(syncTarget.get())
+          } else {
+            syncCompleteHanders.notifySubscribers(completedSyncTarget)
           }
         }
       }
