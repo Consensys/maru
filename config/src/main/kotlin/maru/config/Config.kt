030a054390adeb95ef3819c9d9df56a2337c8580
Jones Ho <jones.ho@consensys.net>
2025-08-15 17:51:53 +0800

    feat: add configs to control TargetChainHeadCalculator and DownloadPeerProvider

--- config/src/main/kotlin/maru/config/Config.kt
+++ config/src/main/kotlin/maru/config/Config.kt
@@ -170,6 +170,7 @@ data class SyncingConfig(
   val peerChainHeightPollingInterval: Duration,
   val peerChainHeightGranularity: UInt,
   val elSyncStatusRefreshInterval: Duration,
+  val useUnconditionalRandomDownloadPeer: Boolean = false,
   val download: Download? = Download(),
 ) {
   data class Download(
