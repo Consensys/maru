1471fd431214a7d2c4ad5704658b82170220fdf0
Jones Ho <jones.ho@consensys.net>
2025-08-15 17:51:53 +0800

    feat: add configs to control TargetChainHeadCalculator and DownloadPeerProvider

--- syncing/src/main/kotlin/maru/syncing/SyncController.kt
+++ syncing/src/main/kotlin/maru/syncing/SyncController.kt
@@ -183,7 +183,7 @@ class BeaconSyncControllerImpl(
       beaconChain: BeaconChain,
       elManager: ExecutionLayerManager,
       peersHeadsProvider: PeersHeadBlockProvider,
-      targetChainHeadCalculator: SyncTargetSelector = HighestHeadTargetSelector(),
+      targetChainHeadCalculator: SyncTargetSelector,
       peerChainTrackerConfig: PeerChainTracker.Config,
       validatorProvider: ValidatorProvider,
       peerLookup: PeerLookup,
@@ -192,6 +192,7 @@ class BeaconSyncControllerImpl(
       elSyncServiceConfig: ELSyncService.Config,
       finalizationProvider: FinalizationProvider,
       allowEmptyBlocks: Boolean = true,
+      useUnconditionalRandomDownloadPeer: Boolean = false,
     ): SyncController {
       val clSyncService =
         CLSyncServiceImpl(
@@ -200,7 +201,10 @@ class BeaconSyncControllerImpl(
           allowEmptyBlocks = allowEmptyBlocks,
           executorService =
             Executors.newCachedThreadPool(Thread.ofPlatform().daemon().factory()),
-          pipelineConfig = BeaconChainDownloadPipelineFactory.Config(),
+          pipelineConfig =
+            BeaconChainDownloadPipelineFactory.Config(
+              useUnconditionalRandomDownloadPeer = useUnconditionalRandomDownloadPeer,
+            ),
           peerLookup = peerLookup,
           besuMetrics = besuMetrics,
           metricsFacade = metricsFacade,
