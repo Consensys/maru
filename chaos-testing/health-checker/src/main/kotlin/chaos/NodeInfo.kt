301f5e9cc4cadb2b27a3d455f034ae722b484a76
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-08-20 16:41:18 +0100

    chaos: fix spotless

--- chaos-testing/health-checker/src/main/kotlin/chaos/NodeInfo.kt
+++ chaos-testing/health-checker/src/main/kotlin/chaos/NodeInfo.kt
@@ -6,10 +6,13 @@
  *
  * SPDX-License-Identifier: MIT OR Apache-2.0
  */
-
 package chaos
 
-data class NodeInfo<T>(val label: String, val value: T) {
+data class NodeInfo<T>(
+  val label: String,
+  val value: T,
+) {
   fun <R> map(fn: (T) -> R): NodeInfo<R> = NodeInfo(label, fn(value))
+
   fun <R> map(newValue: R): NodeInfo<R> = NodeInfo(label, newValue)
 }
