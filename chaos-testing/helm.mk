b953d964512a620008d005c14aaa6f2620888f68
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-08-21 16:04:15 +0100

    chaos: explicitly set default namespace

--- chaos-testing/helm.mk
+++ chaos-testing/helm.mk
@@ -36,8 +36,8 @@ wait-for-log-entry:
 helm-deploy-besu:
 		@echo "Deploying Besu"
 		@kubectl config current-context
-		@helm --kubeconfig $(KUBECONFIG) upgrade --install besu-sequencer ./helm/charts/besu --force -f ./helm/charts/besu/values.yaml -f ./helm/values/besu-local-dev-sequencer.yaml
-		@helm --kubeconfig $(KUBECONFIG) upgrade --install besu-follower ./helm/charts/besu --force -f ./helm/charts/besu/values.yaml -f ./helm/values/besu-local-dev-follower.yaml
+		@helm --kubeconfig $(KUBECONFIG) upgrade --install besu-sequencer ./helm/charts/besu --force -f ./helm/charts/besu/values.yaml -f ./helm/values/besu-local-dev-sequencer.yaml --namespace default
+		@helm --kubeconfig $(KUBECONFIG) upgrade --install besu-follower ./helm/charts/besu --force -f ./helm/charts/besu/values.yaml -f ./helm/values/besu-local-dev-follower.yaml --namespace default
 		@echo "Waiting for Besu to be ready..."
 		@$(MAKE) wait_pods pod_name=besu-sequencer pod_count=1
 		@$(MAKE) wait_pods pod_name=besu-follower pod_count=3
@@ -57,15 +57,15 @@ helm-redeploy-maru:
 	-@helm --kubeconfig $(KUBECONFIG) uninstall maru-follower-3
 	@sleep 2 # Wait for a second to ensure the previous release is fully uninstalled
 	@echo "Deploying Maru Nodes: IMAGE_ARG='$(IMAGE_ARG)'"
-	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-bootnode-0 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-bootnode-0.yaml $(IMAGE_ARG);
+	@helm --kubeconfig $(KUBECONFIG) upgrade --install maru-bootnode-0 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-bootnode-0.yaml --namespace default $(IMAGE_ARG);
 	@$(MAKE) wait_pods pod_name=maru-bootnode-0 pod_count=1
 	@$(MAKE) wait-for-log-entry pod_name=maru-bootnode-0-0 log_entry="enr"
 	@BOOTNODE_ENR=$$(kubectl logs maru-bootnode-0-0 | grep -Ev '0.0.0.0|127.0.0.1' | grep -o 'enr=[^ ]*' | head -1 | cut -d= -f2); \
 	echo "Bootnode ENR: $$BOOTNODE_ENR"; \
-	helm --kubeconfig $(KUBECONFIG) upgrade --install maru-validator ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-validator.yaml --set bootnodes=$$BOOTNODE_ENR $(IMAGE_ARG) ; \
-	helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-1 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-follower-1.yaml --set bootnodes=$$BOOTNODE_ENR $(IMAGE_ARG) ; \
-	helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-2 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-follower-2.yaml --set bootnodes=$$BOOTNODE_ENR $(IMAGE_ARG) ; \
-	helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-3 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-follower-3.yaml --set bootnodes=$$BOOTNODE_ENR $(IMAGE_ARG)
+	helm --kubeconfig $(KUBECONFIG) upgrade --install maru-validator ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-validator.yaml   --namespace default --set bootnodes=$$BOOTNODE_ENR $(IMAGE_ARG) ; \
+	helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-1 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-follower-1.yaml --namespace default --set bootnodes=$$BOOTNODE_ENR $(IMAGE_ARG) ; \
+	helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-2 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-follower-2.yaml --namespace default --set bootnodes=$$BOOTNODE_ENR $(IMAGE_ARG) ; \
+	helm --kubeconfig $(KUBECONFIG) upgrade --install maru-follower-3 ./helm/charts/maru --force -f ./helm/charts/maru/values.yaml -f ./helm/values/maru-local-dev-follower-3.yaml --namespace default --set bootnodes=$$BOOTNODE_ENR $(IMAGE_ARG)
 	@$(MAKE) wait_pods pod_name=maru-validator pod_count=1
 
 helm-redeploy-maru-and-besu:
