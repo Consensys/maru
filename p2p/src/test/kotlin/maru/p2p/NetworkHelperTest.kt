6bcbb565cf2c4de24b9f6fa1dbf95a0ea2104f6c
Roman <4833306+Filter94@users.noreply.github.com>
2025-08-28 18:01:15 +0200

    Fixing ip bind issue

--- p2p/src/test/kotlin/maru/p2p/NetworkHelperTest.kt
+++ p2p/src/test/kotlin/maru/p2p/NetworkHelperTest.kt
@@ -87,12 +87,4 @@ class NetworkHelperTest {
       .isInstanceOf(IllegalArgumentException::class.java)
       .hasMessageContaining("Invalid targetIpV4=192.0.A.A")
   }
-
-  @Test
-  fun `selectIpV4ForP2P throws if targetIpV4 is not present`() {
-    val fakeIp = "192.0.2.123" // TEST-NET-1, unlikely to exist
-    assertThatThrownBy { NetworkHelper.selectIpV4ForP2P(fakeIp) }
-      .isInstanceOf(IllegalArgumentException::class.java)
-      .hasMessageContaining("targetIpV4=$fakeIp not found in machine interfaces")
-  }
 }
