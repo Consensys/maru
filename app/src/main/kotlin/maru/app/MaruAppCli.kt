f451724fc5d5daffb29a12f8069bc1a102b3673e
Jones Ho <jones.ho@consensys.net>
2025-11-03 15:57:45 +0800

    feat: add test case

--- app/src/main/kotlin/maru/app/MaruAppCli.kt
+++ app/src/main/kotlin/maru/app/MaruAppCli.kt
@@ -14,12 +14,14 @@ import maru.config.MaruConfigLoader
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.core.LoggerContext
 import org.apache.logging.log4j.core.config.Configurator
-import picocli.CommandLine
+import picocli.CommandLine.ArgGroup
 import picocli.CommandLine.Command
+import picocli.CommandLine.ITypeConverter
+import picocli.CommandLine.Option
 
 internal class KebabToEnumConverter<T : Enum<T>>(
   private val enumClass: Class<T>,
-) : CommandLine.ITypeConverter<T> {
+) : ITypeConverter<T> {
   override fun convert(value: String): T {
     // Convert kebab-case to upper snake-case
     val enumName = value.replace('-', '_').uppercase()
@@ -45,21 +47,23 @@ internal class KebabToEnumConverter<T : Enum<T>>(
   optionListHeading = "%nOptions:%n",
   footerHeading = "%n",
 )
-class MaruAppCli : Callable<Int> {
-  @CommandLine.Option(
+class MaruAppCli(
+  private val dryRun: Boolean = false,
+) : Callable<Int> {
+  @Option(
     names = ["--config"],
     paramLabel = "CONFIG.toml,CONFIG.overrides.toml",
     description = ["Configuration files"],
     arity = "1..*",
     required = true,
   )
-  private val configFiles: List<File>? = null
+  val configFiles: List<File>? = null
 
-  @CommandLine.ArgGroup(multiplicity = "0..1", exclusive = true, validate = true)
-  private var genesisOptions: GenesisOptions? = null
+  @ArgGroup(multiplicity = "0..1", exclusive = true, validate = true)
+  var genesisOptions: GenesisOptions? = null
 
   class GenesisOptions(
-    @CommandLine.Option(
+    @Option(
       names = ["--maru-genesis-file", "--genesis-file"],
       paramLabel = "BEACON_GENESIS.json",
       description = [
@@ -68,7 +72,7 @@ class MaruAppCli : Callable<Int> {
       required = false,
     )
     var genesisFile: File? = null,
-    @CommandLine.Option(
+    @Option(
       names = ["--network"],
       paramLabel = "linea-mainnet|linea-sepolia (case-insensitive)",
       description = ["Connects to Linea mainnet or sepolia"],
@@ -86,20 +90,19 @@ class MaruAppCli : Callable<Int> {
 
   override fun call(): Int {
     for (configFile in configFiles!!) {
-      if (!validateConfigFile(configFile)) {
+      if (!validateFileCanRead(configFile)) {
         System.err.println("Failed to read config file: \"${configFile.path}\"")
         return 1
       }
     }
-    val parsedAppConfig = MaruConfigLoader.loadAppConfigs(configFiles)
 
     // If "--genesis-file" and "--network" are not specified, default to set "--network=linea-mainnet"
     if (genesisOptions == null) {
       genesisOptions = GenesisOptions(network = Network.LINEA_MAINNET)
     }
     if (genesisOptions!!.genesisFile != null) {
-      if (!validateConfigFile(genesisOptions!!.genesisFile!!)) {
-        System.err.println("Failed to read genesis file file: \"${genesisOptions!!.genesisFile!!.path}\"")
+      if (!validateFileCanRead(genesisOptions!!.genesisFile!!)) {
+        System.err.println("Failed to read genesis file: \"${genesisOptions!!.genesisFile!!.path}\"")
         return 1
       }
       println("Using the given genesis file from \"${genesisOptions!!.genesisFile!!.path}\"")
@@ -108,31 +111,34 @@ class MaruAppCli : Callable<Int> {
         File("/beacon-genesis-files/${genesisOptions!!.network!!.networkNameInKebab}-genesis.json")
       println("Using the genesis file of the named network \"${genesisOptions!!.network!!.networkNameInKebab}\"")
     }
-    val parsedBeaconGenesisConfig = MaruConfigLoader.loadGenesisConfig(genesisOptions!!.genesisFile!!)
 
-    val app =
-      MaruAppFactory()
-        .create(
-          config = parsedAppConfig.domainFriendly(),
-          beaconGenesisConfig = parsedBeaconGenesisConfig.domainFriendly(),
+    if (!dryRun) {
+      val parsedAppConfig = MaruConfigLoader.loadAppConfigs(configFiles)
+      val parsedBeaconGenesisConfig = MaruConfigLoader.loadGenesisConfig(genesisOptions!!.genesisFile!!)
+      val app =
+        MaruAppFactory()
+          .create(
+            config = parsedAppConfig.domainFriendly(),
+            beaconGenesisConfig = parsedBeaconGenesisConfig.domainFriendly(),
+          )
+      app.start()
+
+      Runtime
+        .getRuntime()
+        .addShutdownHook(
+          Thread {
+            app.stop()
+            if (LogManager.getContext() is LoggerContext) {
+              // Disable log4j auto shutdown hook is not used otherwise
+              // Messages in App.stop won't appear in the logs
+              Configurator.shutdown(LogManager.getContext() as LoggerContext)
+            }
+          },
         )
-    app.start()
-
-    Runtime
-      .getRuntime()
-      .addShutdownHook(
-        Thread {
-          app.stop()
-          if (LogManager.getContext() is LoggerContext) {
-            // Disable log4j auto shutdown hook is not used otherwise
-            // Messages in App.stop won't appear in the logs
-            Configurator.shutdown(LogManager.getContext() as LoggerContext)
-          }
-        },
-      )
+    }
 
     return 0
   }
 
-  private fun validateConfigFile(file: File): Boolean = file.canRead()
+  private fun validateFileCanRead(file: File): Boolean = dryRun || file.canRead()
 }
