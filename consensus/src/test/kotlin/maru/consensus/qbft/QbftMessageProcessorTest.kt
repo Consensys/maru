76166bea1e266983518787515a491cfc5aee757a
Jason Frame <jason.frame@consensys.net>
2025-11-04 16:23:05 +1000

    use real BftEventQueue

--- consensus/src/test/kotlin/maru/consensus/qbft/QbftMessageProcessorTest.kt
+++ consensus/src/test/kotlin/maru/consensus/qbft/QbftMessageProcessorTest.kt
@@ -8,6 +8,7 @@
  */
 package maru.consensus.qbft
 
+import java.util.concurrent.TimeUnit
 import maru.consensus.qbft.adapters.QbftBlockAdapter
 import maru.consensus.qbft.adapters.QbftBlockCodecAdapter
 import maru.consensus.qbft.adapters.QbftBlockchainAdapter
@@ -21,7 +22,6 @@ import org.apache.tuweni.bytes.Bytes.EMPTY
 import org.assertj.core.api.Assertions.assertThat
 import org.hyperledger.besu.consensus.common.bft.BftEventQueue
 import org.hyperledger.besu.consensus.common.bft.ConsensusRoundIdentifier
-import org.hyperledger.besu.consensus.common.bft.events.BftEvent
 import org.hyperledger.besu.consensus.common.bft.payload.SignedData
 import org.hyperledger.besu.consensus.qbft.core.messagedata.ProposalMessageData
 import org.hyperledger.besu.consensus.qbft.core.messagewrappers.Proposal
@@ -32,10 +32,7 @@ import org.hyperledger.besu.datatypes.Address
 import org.hyperledger.besu.ethereum.p2p.rlpx.wire.MessageData
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
-import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.never
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class QbftMessageProcessorTest {
@@ -46,7 +43,7 @@ class QbftMessageProcessorTest {
 
   private val blockChain = mock<QbftBlockchainAdapter>()
   private val validatorProvider = mock<QbftValidatorProviderAdapter>()
-  private val bftEventQueue = mock<BftEventQueue>()
+  private val bftEventQueue = BftEventQueue(10)
 
   private val messageProcessor =
     QbftMessageProcessor(
@@ -55,6 +52,10 @@ class QbftMessageProcessorTest {
       localAddress = localAddress,
       bftEventQueue = bftEventQueue,
     )
+
+  init {
+    bftEventQueue.start()
+  }
 
   @Test
   fun `should ignore old messages without adding to queue`() {
@@ -65,7 +66,7 @@ class QbftMessageProcessorTest {
 
     assertThat(result.code).isEqualTo(ValidationResultCode.IGNORE)
     assertThat(result).isInstanceOf(Ignore::class.java)
-    verify(bftEventQueue, never()).add(any())
+    assertThat(bftEventQueue.isEmpty).isTrue()
   }
 
   @Test
@@ -76,9 +77,8 @@ class QbftMessageProcessorTest {
     val result = messageProcessor.handleMessage(qbftMessage).get()
 
     assertThat(result.code).isEqualTo(ValidationResultCode.IGNORE)
-    val bftEventCaptor = argumentCaptor<BftEvent>()
-    verify(bftEventQueue).add(bftEventCaptor.capture())
-    assertThat((bftEventCaptor.firstValue as QbftReceivedMessageEvent).message).isEqualTo(qbftMessage)
+    val bftEvent = bftEventQueue.poll(100, TimeUnit.MILLISECONDS)
+    assertThat((bftEvent as QbftReceivedMessageEvent).message).isEqualTo(qbftMessage)
   }
 
   @Test
@@ -93,9 +93,8 @@ class QbftMessageProcessorTest {
     val result = messageProcessor.handleMessage(qbftMessage).get()
 
     assertThat(result.code).isEqualTo(ValidationResultCode.ACCEPT)
-    val bftEventCaptor = argumentCaptor<BftEvent>()
-    verify(bftEventQueue).add(bftEventCaptor.capture())
-    assertThat((bftEventCaptor.firstValue as QbftReceivedMessageEvent).message).isEqualTo(qbftMessage)
+    val bftEvent = bftEventQueue.poll(100, TimeUnit.MILLISECONDS)
+    assertThat((bftEvent as QbftReceivedMessageEvent).message).isEqualTo(qbftMessage)
   }
 
   @Test
@@ -111,7 +110,7 @@ class QbftMessageProcessorTest {
     val qbftMessage = createQbftMessage(100L)
     val result = messageProcessor.handleMessage(qbftMessage).get()
     assertThat(result.code).isEqualTo(ValidationResultCode.REJECT)
-    verify(bftEventQueue, never()).add(any())
+    assertThat(bftEventQueue.isEmpty).isTrue()
   }
 
   @Test
@@ -126,7 +125,7 @@ class QbftMessageProcessorTest {
     val result = messageProcessor.handleMessage(qbftMessage).get()
 
     assertThat(result.code).isEqualTo(ValidationResultCode.IGNORE)
-    verify(bftEventQueue, never()).add(any())
+    assertThat(bftEventQueue.isEmpty).isTrue()
   }
 
   @Test
@@ -139,7 +138,7 @@ class QbftMessageProcessorTest {
     val result = messageProcessor.handleMessage(qbftMessage).get()
 
     assertThat(result.code).isEqualTo(ValidationResultCode.REJECT)
-    verify(bftEventQueue, never()).add(any())
+    assertThat(bftEventQueue.isEmpty).isTrue()
   }
 
   private fun createQbftMessage(sequenceNumber: Long): QbftMessage {
