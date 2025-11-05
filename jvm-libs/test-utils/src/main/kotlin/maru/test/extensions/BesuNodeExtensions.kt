fe53e1152d95c79a6846ddd6794b228f70e170d4
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-11-05 09:27:10 +0000

    improv cluster tests parallelization

--- jvm-libs/test-utils/src/main/kotlin/maru/test/extensions/BesuNodeExtensions.kt
+++ jvm-libs/test-utils/src/main/kotlin/maru/test/extensions/BesuNodeExtensions.kt
@@ -16,13 +16,29 @@ import org.web3j.protocol.core.methods.response.EthBlock
 
 fun BesuCluster.nodeHeads(): Map<String, ULong> = this.nodes.mapValues { it.value.latestBlockNumber() }
 
-fun BesuNode.latestBlock(includeTransactions: Boolean = false): EthBlock.Block =
-  this
-    .nodeRequests()
-    .eth()
-    .ethGetBlockByNumber(DefaultBlockParameter.valueOf("latest"), includeTransactions)
-    .send()
-    .block
+fun BesuNode.latestBlock(includeTransactions: Boolean = false): EthBlock.Block {
+  // Besu sometime returns null for latest block, when at start up retry a few times to avoid flaky teste
+  var retries = 5
+  var block: EthBlock.Block? = null
+  while (retries > 0 && block == null) {
+    try {
+      block =
+        this
+          .nodeRequests()
+          .eth()
+          .ethGetBlockByNumber(DefaultBlockParameter.valueOf("latest"), includeTransactions)
+          .send()
+          .block
+    } catch (e: Exception) {
+      // ignore and retry
+    }
+    if (block == null) {
+      retries--
+      Thread.sleep(200)
+    }
+  }
+  return block ?: throw RuntimeException("Could not retrieve latest block after retries")
+}
 
 fun BesuNode.latestBlockNumber(): ULong =
   this
