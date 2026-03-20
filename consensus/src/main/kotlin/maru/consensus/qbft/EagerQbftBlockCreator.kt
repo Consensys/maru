b515860cd7579f60a65491ce4908fd240f3dcbb2
Roman <4833306+Filter94@users.noreply.github.com>
2026-03-20 12:00:19 +0100

    Added back forgotten EagerBlockCreator

---
+++ consensus/src/main/kotlin/maru/consensus/qbft/EagerQbftBlockCreator.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * This file is dual-licensed under either the MIT license or Apache License 2.0.
+ * See the LICENSE-MIT and LICENSE-APACHE files in the repository root for details.
+ *
+ * SPDX-License-Identifier: MIT OR Apache-2.0
+ */
+package maru.consensus.qbft
+
+import kotlin.time.Duration
+import maru.consensus.PrevRandaoProvider
+import maru.consensus.qbft.adapters.toBeaconBlockHeader
+import maru.consensus.state.FinalizationProvider
+import maru.database.BeaconChain
+import maru.executionlayer.manager.ExecutionLayerManager
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.hyperledger.besu.consensus.qbft.core.types.QbftBlock
+import org.hyperledger.besu.consensus.qbft.core.types.QbftBlockCreator
+import org.hyperledger.besu.consensus.qbft.core.types.QbftBlockHeader
+import org.hyperledger.besu.crypto.SECPSignature
+
+/**
+ * Responsible for QBFT block creation. As opposed to DelayedQbftBlockCreator, Eager one will send the FCU request to
+ * the execution client to start the block building process and will wait for the required time until a block is built,
+ * blocking the thread
+ */
+class EagerQbftBlockCreator(
+  private val manager: ExecutionLayerManager,
+  private val delegate: QbftBlockCreator,
+  private val finalizationStateProvider: FinalizationProvider,
+  private val prevRandaoProvider: PrevRandaoProvider<ULong>,
+  private val feeRecipient: ByteArray,
+  private val beaconChain: BeaconChain,
+  private val config: Config,
+) : QbftBlockCreator {
+  private val log: Logger = LogManager.getLogger(this.javaClass)
+
+  data class Config(
+    val minBlockBuildTime: Duration,
+  )
+
+  override fun createBlock(
+    headerTimeStampSeconds: Long,
+    parentHeader: QbftBlockHeader,
+  ): QbftBlock {
+    val beaconBlockHeader = parentHeader.toBeaconBlockHeader()
+    val parentBeaconBlock =
+      beaconChain
+        .getSealedBeaconBlock(beaconBlockHeader.hash)
+        ?.beaconBlock
+        ?: throw IllegalStateException("Parent block not found in the database")
+    val finalizedState = finalizationStateProvider(parentBeaconBlock.beaconBlockBody)
+
+    val headHash =
+      if (parentBeaconBlock.beaconBlockHeader.number == 0UL) {
+        manager.getLatestBlockHash().get()
+      } else {
+        parentBeaconBlock.beaconBlockBody.executionPayload.blockHash
+      }
+
+    val blockBuildingTriggerResult =
+      manager
+        .setHeadAndStartBlockBuilding(
+          headHash = headHash,
+          safeHash = finalizedState.safeBlockHash,
+          finalizedHash = finalizedState.finalizedBlockHash,
+          nextBlockTimestamp = headerTimeStampSeconds.toULong(),
+          feeRecipient = feeRecipient,
+          prevRandao =
+            prevRandaoProvider.calculateNextPrevRandao(
+              signee =
+                parentBeaconBlock.beaconBlockBody.executionPayload.blockNumber
+                  .inc(),
+              prevRandao = parentBeaconBlock.beaconBlockBody.executionPayload.prevRandao,
+            ),
+        ).get()
+    log.debug(
+      "Building new block, FCU result={}",
+      blockBuildingTriggerResult,
+    )
+    val sleepTime = config.minBlockBuildTime.inWholeMilliseconds
+    log.debug("Block building has started, sleeping for {} milliseconds", sleepTime)
+    Thread.sleep(sleepTime)
+    log.debug("Block building has finished, time to collect block building results")
+    return delegate.createBlock(headerTimeStampSeconds, parentHeader)
+  }
+
+  override fun createSealedBlock(
+    block: QbftBlock,
+    roundNumber: Int,
+    commitSeals: Collection<SECPSignature>,
+  ): QbftBlock = DelayedQbftBlockCreator.createSealedBlock(block, roundNumber, commitSeals)
+}
